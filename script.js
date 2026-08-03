17a415f88623f642b2bc9c36c0ed6e875b292a42
ccwhitfield06 <ccwhitfield06@gmail.com>
2026-08-03 10:40:41 -0500

    Add park search functionality

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ buttons.forEach(button => {
         button.textContent = "Saved!";
     });
 });
+
+
+const searchBar = document.getElementById("searchBar");
+const parks = document.querySelectorAll(".park-card");
+
+searchBar.addEventListener("keyup", () => {
+    const searchText = searchBar.value.toLowerCase();
+
+    parks.forEach(park => {
+        const parkName = park.querySelector("h2").textContent.toLowerCase();
+
+        if (parkName.includes(searchText)) {
+            park.style.display = "block";
+        } else {
+            park.style.display = "none";
+        }
+    });
+});
