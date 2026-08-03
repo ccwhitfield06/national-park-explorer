79ffae0f9c7cfc55b43fe133b5928f49521de31a
ccwhitfield06 <ccwhitfield06@gmail.com>
2026-08-03 11:09:49 -0500

    Add favorite and visited park storage

--- script.js
+++ script.js
@@ -1,25 +1,104 @@
-const buttons = document.querySelectorAll("button");
+const favoriteButtons = document.querySelectorAll(".park-card button:not(.visit-button)");
+const visitButtons = document.querySelectorAll(".visit-button");
 
-buttons.forEach(button => {
+
+favoriteButtons.forEach(button => {
+
     button.addEventListener("click", () => {
-        button.textContent = "Saved!";
+
+        const card = button.closest(".park-card");
+        const parkName = card.querySelector("h2").textContent;
+
+
+        let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+
+        if (!favorites.includes(parkName)) {
+
+            favorites.push(parkName);
+
+            localStorage.setItem(
+                "favorites",
+                JSON.stringify(favorites)
+            );
+
+            button.textContent = "Saved!";
+
+        }
+
     });
+
 });
+
+
+
+
+visitButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const card = button.closest(".park-card");
+        const parkName = card.querySelector("h2").textContent;
+
+
+        let visited = JSON.parse(localStorage.getItem("visited")) || [];
+
+
+        if (!visited.includes(parkName)) {
+
+            visited.push(parkName);
+
+            localStorage.setItem(
+                "visited",
+                JSON.stringify(visited)
+            );
+
+            button.textContent = "Visited!";
+
+        }
+
+    });
+
+});
+
+
+
 
 
 const searchBar = document.getElementById("searchBar");
-const parks = document.querySelectorAll(".park-card");
 
-searchBar.addEventListener("keyup", () => {
-    const searchText = searchBar.value.toLowerCase();
 
-    parks.forEach(park => {
-        const parkName = park.querySelector("h2").textContent.toLowerCase();
+if (searchBar) {
 
-        if (parkName.includes(searchText)) {
-            park.style.display = "block";
-        } else {
-            park.style.display = "none";
-        }
+    const parks = document.querySelectorAll(".park-card");
+
+
+    searchBar.addEventListener("keyup", () => {
+
+        const searchText =
+        searchBar.value.toLowerCase();
+
+
+        parks.forEach(park => {
+
+            const name =
+            park.querySelector("h2")
+            .textContent
+            .toLowerCase();
+
+
+            if(name.includes(searchText)) {
+
+                park.style.display = "block";
+
+            } else {
+
+                park.style.display = "none";
+
+            }
+
+        });
+
     });
-});
+
+}
